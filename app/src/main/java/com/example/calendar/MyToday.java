928164f2d6994fe46555f29fb1344a7176b1801d
Ruchkina <ruch.nastya@gmail.com>
2024-02-12 19:49:17 +0300

    make_calendar_as_activity

--- app/src/main/java/com/example/calendar/MyToday.java
+++ app/src/main/java/com/example/calendar/MyToday.java
@@ -18,7 +18,7 @@ public class MyToday extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.today, container, false);
         TextView textView = v.findViewById(R.id.textView);
-        textView.setText(GlobalYear );
+        textView.setText(GlobalYear);
         return v;
     }
 }
